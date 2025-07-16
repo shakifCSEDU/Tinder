d7bb3f4678099c86e3b5b86277536c94a3f3cd0a
shakif <sakifsahriar@gmail.com>
2025-07-16 13:17:33 +0600

    add validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator')
+
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -13,6 +15,11 @@ const userSchema = new mongoose.Schema({
         required: true,
         lowercase: true,
         trim : true,
+        validate(value){
+          if(!validator.isEmail("value")){
+              throw new Error("Invalid email address :"+value);
+          }
+        },
     },
     password:{
         type: String,
