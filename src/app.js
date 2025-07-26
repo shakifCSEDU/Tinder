2f950abf9905446ff89343239f65c1a74c269b3a
shakif <sakifsahriar@gmail.com>
2025-07-26 12:46:22 +0600

    add cors and configured

--- src/app.js
+++ src/app.js
@@ -3,10 +3,15 @@ const responseTime = require('response-time');
 const connectDB = require("./config/database");
 const app = express();
 const PORT = process.env.PORT || 8000;
+const cors = require("cors");
 
 const cookieParser = require('cookie-parser');
 
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser())
 const authRouter = require("./routes/auth");
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -39,7 +39,7 @@ authRouter.post("/login", async (req, res) => {
             const token = await user.getJWT();
 
             res.cookie("token", token, {expires: new Date(Date.now() + 8* 3600000)});
-            res.send("Login Successfull");
+            res.send(user);
         } else {
             throw new Error("Password is not correct");
         }
