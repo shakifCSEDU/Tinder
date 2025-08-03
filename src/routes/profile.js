14423bc5dcd3f4ee8d981d99179189662e9757e9
shakif <sakifsahriar@gmail.com>
2025-08-03 21:54:48 +0600

    update profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -17,9 +17,9 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
 });
 profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     try{
-        if(!validateEditProfileData(req)){
-            throw new Error("Invalid edit request");
-        }
+        // if(!validateEditProfileData(req)){
+        //     throw new Error("Invalid edit request");
+        // }
         const loggedInUser = req.user;
 
         Object.keys(req.body).forEach((key) => {
